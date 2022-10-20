33b36836a28d41a8cafdae6425fd751230011550
manoj1441 <108724548+manoj1441@users.noreply.github.com>
2022-10-20 14:27:18 +0530

    Updates

---
+++ precache-manifest.39fcfc33c30072a33dd7fa9fb7fe58f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9a278f6920a2e4ff78f",
+    "url": "/BreakingBad/static/css/main.deeccd54.chunk.css"
+  },
+  {
+    "revision": "f9a278f6920a2e4ff78f",
+    "url": "/BreakingBad/static/js/main.b8ec4a87.chunk.js"
+  },
+  {
+    "revision": "a2f02901a3df2f5d5b49",
+    "url": "/BreakingBad/static/js/runtime~main.ec9130e5.js"
+  },
+  {
+    "revision": "3d11c6f9b469db31c735",
+    "url": "/BreakingBad/static/js/2.392b2108.chunk.js"
+  },
+  {
+    "revision": "ab10911e2d5b3797c702d85ec55a9e99",
+    "url": "/BreakingBad/static/media/BB.ab10911e.jpg"
+  },
+  {
+    "revision": "96fd30ac612b2ca18def19dd6bb7d97e",
+    "url": "/BreakingBad/index.html"
+  }
+];
